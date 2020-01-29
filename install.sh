6be62506bdf91703d54ac2ac09eb073a9ff4cb79
Lucas Guedes <lucasgeds@gmail.com>
2020-01-29 19:22:39 -0300

    install.sh: forcing symlinks creation when they already exist

--- install.sh
+++ install.sh
@@ -5,21 +5,21 @@ if [ -f ~/.zshrc ]; then
 	mv ~/.zshrc ~/.zshrc.bkp
 fi
 echo "Symlinking ~/.zshrc..."
-ln -s $script_dir/.zshrc ~/.zshrc
+ln -sf $script_dir/.zshrc ~/.zshrc
 
 if [ -f ~/.gitignore ]; then
 	echo "Moving ~/.gitignore to ~/.gitignore.bkp..."
 	mv ~/.gitignore ~/.gitignore.bkp
 fi
 echo "Symlinking ~/.gitignore..."
-ln -s $script_dir/.gitignore ~/.gitignore
+ln -sf $script_dir/.gitignore ~/.gitignore
 
 if [ -f ~/.tool-versions ]; then
 	echo "Moving ~/.tool-versions to ~/.tool-versions.bkp..."
 	mv ~/.tool-versions ~/.tool-versions.bkp
 fi
 echo "Symlinking ~/.tool-versions..."
-ln -s $script_dir/.tool-versions ~/.tool-versions
+ln -sf $script_dir/.tool-versions ~/.tool-versions
 
 if [ -f ~/.vimrc ]; then
 	echo "Moving ~/.vimrc to ~/.vimrc.bkp..."
@@ -30,8 +30,8 @@ if [ -f ~/.config/nvim/init.vim ]; then
 	mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bkp
 fi
 echo "Symlinking ~/.vimrc..."
-ln -s $script_dir/.vimrc ~/.vimrc
+ln -sf $script_dir/.vimrc ~/.vimrc
 echo "Symlinking ~/.config/nvim/init.vim..."
-ln -s $script_dir/.vimrc ~/.config/nvim/init.vim
+ln -sf $script_dir/.vimrc ~/.config/nvim/init.vim
 
 echo "Done!"
